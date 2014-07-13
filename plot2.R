278f16141fc69ef5a0fc9acf92528f82904a7383
marcolunardi <marcolunardi@mbp-di-marco-2.lan>
2014-07-13 15:26:58 +0200

    Assignment 1
    Assignment 1

--- plot2.R
+++ plot2.R
@@ -2,7 +2,7 @@ library(datasets)
 png(filename="plot2.png",width=480,height=480)
 
 ## reads only rows with selected dates from data file
-powerdata <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), header=F, sep=";", na.string="?")
+powerdata <- na.omit(read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), header=F, sep=";", na.string="?"))
 
 ## adds header to data subset
 colnames(powerdata) <- names(read.table('household_power_consumption.txt', header=TRUE, sep=";", nrows=1))
